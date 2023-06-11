0c07262c27986c7968fd1c04ea28ef56fc632d8e
christina <headsteins@gmail.com>
2023-06-11 13:14:46 +0800

    fix codes for reducing diff on platform

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ use iced::window::Event as WindowEvent;
 use iced::Event;
 use iced::{Alignment, Application, Command, Element, Length, Settings, Subscription, Theme};
 
+extern crate dirs;
+
 const FONT: &[u8] = include_bytes!(concat!(
     env!("CARGO_MANIFEST_DIR"),
     "/resource/SourceHanSansCN-Regular.otf"
@@ -50,9 +52,15 @@ enum VideoProcessor {
 }
 
 #[derive(Debug, Clone)]
+enum VideoType {
+    MP4,
+    GIF,
+}
+
+#[derive(Debug, Clone)]
 enum Message {
     EventOccurred(Event),
-    Submit(String),
+    Submit(VideoType),
     FfmpegComplete(PathBuf),
     FfmpegFound(bool),
 }
@@ -169,7 +177,7 @@ fn select_target_view(ctx: &SelectTargetCtx) -> Element<'static, Message> {
             .horizontal_alignment(alignment::Horizontal::Center),
     )
     .width(Length::Fixed(100.))
-    .on_press(Message::Submit("mp4".to_string()));
+    .on_press(Message::Submit(VideoType::MP4));
 
     let button_gif = button(
         text("GIF")
@@ -177,7 +185,7 @@ fn select_target_view(ctx: &SelectTargetCtx) -> Element<'static, Message> {
             .horizontal_alignment(alignment::Horizontal::Center),
     )
     .width(Length::Fixed(100.))
-    .on_press(Message::Submit("gif".to_string()));
+    .on_press(Message::Submit(VideoType::GIF));
 
     let content = Column::new()
         .align_items(Alignment::Center)
@@ -258,7 +266,7 @@ fn error_view() -> Element<'static, Message> {
         .into()
 }
 
-fn _ffmpeg_execute(src_video: PathBuf, video_type: String) -> PathBuf {
+fn _ffmpeg_execute(src_video: PathBuf, video_type: VideoType) -> PathBuf {
     let src_path = src_video.to_str().unwrap();
     let dst_path = {
         let filename_without_suffix = src_video
@@ -267,8 +275,8 @@ fn _ffmpeg_execute(src_video: PathBuf, video_type: String) -> PathBuf {
             .unwrap_or("output");
 
         let dir = PathBuf::new()
-            .join("./")
-            .join("test/dist");
+            .join(dirs::download_dir().unwrap())
+            .join("VideoConvert");
 
         println!("dir: {}", dir.to_str().unwrap());
         if dir.is_file() {
@@ -277,10 +285,13 @@ fn _ffmpeg_execute(src_video: PathBuf, video_type: String) -> PathBuf {
         if !dir.is_dir() {
             fs::create_dir(dir.clone()).expect("create dst dir failed.");
         }
-        if video_type == "mp4" {
-            dir.join(filename_without_suffix.to_owned() + ".mp4")
-        } else {
-            dir.join(filename_without_suffix.to_owned() + ".gif")
+        match video_type {
+            VideoType::MP4 => {
+                dir.join(filename_without_suffix.to_owned() + ".mp4")
+            },
+            VideoType::GIF => {
+                dir.join(filename_without_suffix.to_owned() + ".gif")
+            }
         }
     };
 
@@ -289,18 +300,21 @@ fn _ffmpeg_execute(src_video: PathBuf, video_type: String) -> PathBuf {
     }
 
     let mut command = process::Command::new("ffmpeg");
-    if video_type == "mp4" {
-        command
-        .arg("-i")
-        .arg(src_path)
-        .arg("-vf")
-        .arg("scale=trunc(iw/2)*2:trunc(ih/2)*2")
-        .arg(dst_path.clone());
-    } else {
-        command
-        .arg("-i")
-        .arg(src_path)
-        .arg(dst_path.clone());
+    match video_type {
+        VideoType::MP4 => {
+            command
+            .arg("-i")
+            .arg(src_path)
+            .arg("-vf")
+            .arg("scale=trunc(iw/2)*2:trunc(ih/2)*2")
+            .arg(dst_path.clone());
+        },
+        VideoType::GIF => {
+            command
+            .arg("-i")
+            .arg(src_path)
+            .arg(dst_path.clone());
+        }
     }
 
     println!("{:?}", command);
@@ -319,17 +333,12 @@ fn _ffmpeg_execute(src_video: PathBuf, video_type: String) -> PathBuf {
 fn test_ffmpeg_execute() {
     _ffmpeg_execute(
         PathBuf::new().join("/mnt/yjn/DATA/Videos/录屏/录屏 2023年04月17日 19时13分35秒.webm"),
-        "mp4".to_string()
+        VideoType::MP4
     );
 }
 
-async fn ffmpeg_execute(src_video: PathBuf, video_type: String) -> PathBuf {
-    if video_type == "mp4" {
-        _ffmpeg_execute(src_video, video_type)
-    } else {
-        _ffmpeg_execute(src_video, video_type)
-    }
-    
+async fn ffmpeg_execute(src_video: PathBuf, video_type: VideoType) -> PathBuf {
+    _ffmpeg_execute(src_video, video_type)
 }
 
 fn _ffmpeg_found() -> bool {
